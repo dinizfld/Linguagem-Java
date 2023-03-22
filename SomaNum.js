2fd820fcc38b79a45a57f9367b1a62e5ae77dc38
FERNANDO DINIZ <123640727+dinizfld@users.noreply.github.com>
2023-03-22 16:19:54 -0300

    Create SomaNum.js
    Somar dois números inteiros e imprimir o resultado:

---
+++ SomaNum.js
@@ -0,0 +1,8 @@
+public class Soma {
+   public static void main(String[] args) {
+      int a = 10;
+      int b = 5;
+      int resultado = a + b;
+      System.out.println("A soma de " + a + " e " + b + " é igual a " + resultado);
+   }
+}
