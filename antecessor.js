0d35a9bd68997544be038590b561e7fc0a5a2bf3
FERNANDO DINIZ <123640727+dinizfld@users.noreply.github.com>
2023-03-31 13:06:13 -0300

    Create antecessor.js
    Desenvolva um algoritmo em Java que leia um número inteiro e imprima o seu
    antecessor e seu sucessor.

---
+++ antecessor.js
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class AntecessorSucessor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro: ");
+        int num = sc.nextInt();
+
+        int antecessor = num - 1;
+        int sucessor = num + 1;
+
+        System.out.println("Antecessor: " + antecessor);
+        System.out.println("Sucessor: " + sucessor);
+
+        sc.close();
+    }
+}
